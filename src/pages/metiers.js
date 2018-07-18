fb2e533aa971c38a5b3a84abe9e731b8ab173771
Yves Vinckier <yves.vinckier@live.fr>
2018-07-18 23:14:48 +0200

    last

--- src/pages/metiers.js
+++ src/pages/metiers.js
@@ -4,83 +4,83 @@ import Img from 'gatsby-image'
 import Helmet from 'react-helmet'
 
 class Metiers extends Component {
-    render() {
-        const posts = this.props.data.allContentfulGallery.edges
+  render() {
+    const posts = this.props.data.allContentfulGallery.edges
 
-        return (
-            <div>
-                <Helmet>
-                    <title>Métiers - JEAN EMMANUEL RODE Photographe LILLE</title>
-                    <meta
-                        name='description'
-                        content='Les Métiers par JEAN EMMANUEL RODE Photographe LILLE'
-                    />
-                    <meta
-                        property='og:title'
-                        content='Les Métiers - JEAN EMMANUEL RODE Photographe LILLE'
-                    />
-                    <meta property='og:image' content={posts[0].node.cover.sizes.src} />
-                    <meta property='og:image:width' content='1800' />
-                    <meta property='og:image:height' content='1200' />
-                    <meta
-                        property='og:url'
-                        content='http://www.rode-island.com/metiers/'
-                    />
-                </Helmet>
+    return (
+      <div>
+        <Helmet>
+          <title>Métiers - JEAN EMMANUEL RODE Photographe LILLE</title>
+          <meta
+            name='description'
+            content='Les Métiers par JEAN EMMANUEL RODE Photographe LILLE'
+          />
+          <meta
+            property='og:title'
+            content='Les Métiers - JEAN EMMANUEL RODE Photographe LILLE'
+          />
+          <meta property='og:image' content={posts[0].node.cover.sizes.src} />
+          <meta property='og:image:width' content='1800' />
+          <meta property='og:image:height' content='1200' />
+          <meta
+            property='og:url'
+            content='http://www.rode-island.com/metiers/'
+          />
+        </Helmet>
 
-                <div className='category-navigation'>
-                    <h2>Galeries</h2>
-                    <ul className='category-navigation__links'>
-                        <li>
-                            <Link to='/galeries/'>All</Link>
-                        </li>
-                        <li>
-                            <Link to='/culinaire-sucre/'>
+        <div className='category-navigation'>
+          <h2>Galeries</h2>
+          <ul className='category-navigation__links'>
+            <li>
+              <Link to='/galeries/'>All</Link>
+            </li>
+            <li>
+              <Link to='/culinaire-sucre/'>
                                 Culinaire sucré
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/culinaire-sale/'>Culinaire salé</Link>
-                        </li>
-                        <li>
-                            <Link to='/nature-morte-deco/'>Nature Morte | Déco</Link>
-                        </li>
-                        <li>
-                            <Link to='/spectacle/'>Spectacle</Link>
-                        </li>
-                        <li>
-                            <Link to='/metiers/' className='active'>Métiers</Link>
-                        </li>
-                    </ul>
-                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to='/culinaire-sale/'>Culinaire salé</Link>
+            </li>
+            <li>
+              <Link to='/nature-morte-deco/'>Nature Morte | Déco</Link>
+            </li>
+            <li>
+              <Link to='/spectacle/'>Spectacle</Link>
+            </li>
+            <li>
+              <Link to='/metiers/' className='active'>Métiers</Link>
+            </li>
+          </ul>
+        </div>
 
-                <div>
-                    <ul>
-                        {posts.map(({ node: post, index }) => (
-                            <li key={post.id} className='thumbnail-container'>
-                                <h2>{post.title}</h2>
-                                <Link to={'/' + post.slug + '/'}>
-                                    {/* <Img
+        <div>
+          <ul>
+            {posts.map(({ node: post, index }) => (
+              <li key={post.id} className='thumbnail-container'>
+                <h2>{post.title}</h2>
+                <Link to={'/' + post.slug + '/'}>
+                  {/* <Img
                     sizes={post.cover.sizes}
                     alt={post.cover.title}
                     title={post.cover.title}
                   /> */}
-                                    <div className='thumbnail-images'>
-                                        {post.images &&
+                  <div className='thumbnail-images'>
+                    {post.images &&
                                             post.images.map((images, index) => (
-                                                <div key={index} className='cell--fifth'>
-                                                    <Img sizes={post.images[index].sizes} />
-                                                </div>
+                                              <div key={index} className='cell--fifth'>
+                                                <Img sizes={post.images[index].sizes} />
+                                              </div>
                                             ))}
-                                    </div>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export const query = graphql`
--- src/pages/spectacle.js
+++ src/pages/spectacle.js
@@ -4,81 +4,81 @@ import Img from 'gatsby-image'
 import Helmet from 'react-helmet'
 
 class Spectacle extends Component {
-    render() {
-        const posts = this.props.data.allContentfulGallery.edges
+  render() {
+    const posts = this.props.data.allContentfulGallery.edges
 
-        return (
-            <div>
-                <Helmet>
-                    <title>Spectacle - JEAN EMMANUEL RODE Photographe LILLE</title>
-                    <meta
-                        name='description'
-                        content='Le Spectacle par JEAN EMMANUEL RODE Photographe LILLE'
-                    />
-                    <meta
-                        property='og:title'
-                        content='Le spectacle - JEAN EMMANUEL RODE Photographe LILLE'
-                    />
-                    <meta property='og:image' content={posts[0].node.cover.sizes.src} />
-                    <meta property='og:image:width' content='1800' />
-                    <meta property='og:image:height' content='1200' />
-                    <meta
-                        property='og:url'
-                        content='http://www.rode-island.com/spectacle/'
-                    />
-                </Helmet>
+    return (
+      <div>
+        <Helmet>
+          <title>Spectacle - JEAN EMMANUEL RODE Photographe LILLE</title>
+          <meta
+            name='description'
+            content='Le Spectacle par JEAN EMMANUEL RODE Photographe LILLE'
+          />
+          <meta
+            property='og:title'
+            content='Le spectacle - JEAN EMMANUEL RODE Photographe LILLE'
+          />
+          <meta property='og:image' content={posts[0].node.cover.sizes.src} />
+          <meta property='og:image:width' content='1800' />
+          <meta property='og:image:height' content='1200' />
+          <meta
+            property='og:url'
+            content='http://www.rode-island.com/spectacle/'
+          />
+        </Helmet>
 
-                <div className='category-navigation'>
-                    <h2>Galeries</h2>
-                    <ul className='category-navigation__links'>
-                        <li>
-                            <Link to='/galeries/'>All</Link>
-                        </li>
-                        <li>
-                            <Link to='/culinaire-sucre/'>Culinaire sucré</Link>
-                        </li>
-                        <li>
-                            <Link to='/culinaire-sale/'>Culinaire salé</Link>
-                        </li>
-                        <li>
-                            <Link to='/nature-morte-deco/'>Nature Morte | Déco</Link>
-                        </li>
-                        <li>
-                            <Link to='/spectacle/' className='active'>Spectacle</Link>
-                        </li>
-                        <li>
-                            <Link to='/metiers/'>Métiers</Link>
-                        </li>
-                    </ul>
-                </div>
+        <div className='category-navigation'>
+          <h2>Galeries</h2>
+          <ul className='category-navigation__links'>
+            <li>
+              <Link to='/galeries/'>All</Link>
+            </li>
+            <li>
+              <Link to='/culinaire-sucre/'>Culinaire sucré</Link>
+            </li>
+            <li>
+              <Link to='/culinaire-sale/'>Culinaire salé</Link>
+            </li>
+            <li>
+              <Link to='/nature-morte-deco/'>Nature Morte | Déco</Link>
+            </li>
+            <li>
+              <Link to='/spectacle/' className='active'>Spectacle</Link>
+            </li>
+            <li>
+              <Link to='/metiers/'>Métiers</Link>
+            </li>
+          </ul>
+        </div>
 
-                <div>
-                    <ul>
-                        {posts.map(({ node: post, index }) => (
-                            <li key={post.id} className='thumbnail-container'>
-                                <h2>{post.title}</h2>
-                                <Link to={'/' + post.slug + '/'}>
-                                    {/* <Img
+        <div>
+          <ul>
+            {posts.map(({ node: post, index }) => (
+              <li key={post.id} className='thumbnail-container'>
+                <h2>{post.title}</h2>
+                <Link to={'/' + post.slug + '/'}>
+                  {/* <Img
                     sizes={post.cover.sizes}
                     alt={post.cover.title}
                     title={post.cover.title}
                   /> */}
-                                    <div className='thumbnail-images'>
-                                        {post.images &&
+                  <div className='thumbnail-images'>
+                    {post.images &&
                                             post.images.map((images, index) => (
-                                                <div key={index} className='cell--fifth'>
-                                                    <Img sizes={post.images[index].sizes} />
-                                                </div>
+                                              <div key={index} className='cell--fifth'>
+                                                <Img sizes={post.images[index].sizes} />
+                                              </div>
                                             ))}
-                                    </div>
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export const query = graphql`
