5911f0ccb520daa70dd903b4311fb904b8d0a50d
Yves Vinckier <yves.vinckier@live.fr>
2018-10-29 21:40:51 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ var config = {
   logo: 'TODO',
   name: 'JEAN-EMMANUEL RODE',
   legal_name: 'JEAN-EMMANUEL RODE',
-  url: 'https://www.jeanemmanuelrode.com/',
+  url: 'https://www.jeanemmanuelrode.com',
   phone: PHONE.join('-'),
 
   // Required address object for schema
